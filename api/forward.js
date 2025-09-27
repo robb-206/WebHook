bf32db3b47b406a19fe8510bd64726089c5ec559
robb-206 <rdionian69@gmail.com>
2025-09-27 14:01:52 -0400

    Update forward.js

--- api/forward.js
+++ api/forward.js
@@ -1,4 +1,8 @@
-export default function handler(req, res) {
+import fetch from 'node-fetch';
+
+export default async function handler(req, res) {
+  const azureUrl = 'https://htdev-bvhyhpbzgbg3apdh.canadacentral-01.azurewebsites.net/HottubWebhook';
+
   console.log('Incoming request:', {
     method: req.method,
     headers: req.headers,
@@ -6,5 +10,26 @@ export default function handler(req, res) {
     query: req.query
   });
 
-  res.status(200).send("Function is alive");
+  if (req.method === 'GET') {
+    return res.status(200).send("Function is alive");
+  }
+
+  try {
+    const body = req.body && Object.keys(req.body).length ? JSON.stringify(req.body) : undefined;
+
+    const response = await fetch(azureUrl, {
+      method: req.method,
+      headers: {
+        'Content-Type': req.headers['content-type'] || 'application/json',
+      },
+      body: body,
+    });
+
+    const text = await response.text();
+    res.status(response.status).send(text);
+  } catch (err) {
+    console.error('Forwarding error:', err);
+    res.status(500).send('Error forwarding request: ' + err.message);
+  }
 }
+
